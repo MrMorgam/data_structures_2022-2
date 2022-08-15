9009d0de9d132a10e44eb89728c5fbcb789c75ea
Gabriel Morais <gabriel.am.mor@gmail.com>
2022-08-15 18:41:51 -0300

    ordinary changes

---
+++ 01_exercicio/01_conv_ms_para_kmh.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+float vel_ms;
+float vel_kmph;
+
+int main() {
+    cout<<"Digite a velocidade em m/s: ";
+    cin>>vel_ms;
+
+    vel_kmph = vel_ms * 3.6;
+
+    cout<<"Velocidade: " << vel_kmph << " km/h\n";
+}
