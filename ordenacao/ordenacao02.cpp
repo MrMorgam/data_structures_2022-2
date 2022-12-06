cb1016733f2aa6eaf3d341253402e6854a330d8b
Gabriel Morais <gabriel.am.mor@gmail.com>
2022-12-06 20:48:55 -0300

    ordinary changes

--- ordenacao/ordenacao02.cpp
+++ ordenacao/ordenacao02.cpp
@@ -1,20 +1,55 @@
 #include<stdio.h>
 #include<iostream>
+#define troca(a, b) {int x = a; a = b; b = x; }
+
 using namespace std;
-void Quick(int vetor[10], int inicio, int fim);
+
+void quick_sort(int v[10], int inicio, int fim);
 
 int main(){
-   int vetor[6] = {7, 9, 4, 3,6,1};
-   int i;   
-   Quick(vetor, 0, 5);
-   printf("\n2.Vetor ordenado:\n");
+   int v[6] = {7, 9, 4, 3, 6, 1};
+   int i;
+
+   quick_sort(v, 0, 5);
+
+   printf("Vetor ordenado: ");
    for(i = 0; i <= 5; i++){
-      printf("%d ", vetor[i]);
+      printf("%d ", v[i]);
    }
    printf("\n");   
+
+   return 0;
 }
 
-void Quick(int vetor[10], int inicio, int fim){
-    
+int particiona (int v[], int inicio, int fim) {
+    int x = v[inicio];
+    inicio--;
+    fim++;
+
+   while(inicio < fim){
+      do {
+         fim--; 
+      } while(v[fim] > x);
+      
+      do { 
+         inicio++; 
+      } while(v[inicio]<x);
+        
+      if (inicio < fim) {
+         troca(v[inicio], v[fim]);
+      }
+   }
+
+   return fim;
 }
 
+void quick_sort(int v[10], int inicio, int fim){
+   if(inicio >= fim) {
+      return;
+   }
+
+   int meio = particiona(v, inicio, fim);
+
+   quick_sort(v, inicio, meio);
+   quick_sort(v, meio + 1, fim);
+}
